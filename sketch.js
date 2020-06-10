650b2b2a9956c21c70dcdfe4c9f2eed7b303f7dc
Jack Graham <jackbanna@gmail.com>
2020-06-10 10:31:26 -0300

    initial commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB, 100);
+	noLoop();
+	randomSeed(1);
+	noiseSeed(1);
+}
+
+function draw() {
+	background(50, 50, 100);
+}
